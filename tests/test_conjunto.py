24e855663e64a87849e9baa85344edc4369c8ee1
dani-sie <dani-sie@uniandes.edu.co>
2024-09-17 16:23:11 -0500

    HU001-CASO1: Calcular promedio con un conjunto vacio

--- tests/test_conjunto.py
+++ tests/test_conjunto.py
@@ -2,4 +2,7 @@ import unittest
 from KataTDD.conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
-    pass
+    
+    def test_conjunto_vacio(self):
+        newConjunto = Conjunto([])
+        self.assertIsNone(newConjunto.promedio())
